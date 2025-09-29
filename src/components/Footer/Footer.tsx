a53d5096ba95af3dd2e1a6c460ee1b5306d6fd72
Marixavq <marianaxavierquispe@gmail.com777>
2025-09-28 22:35:23 -0300

    Responsividade footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,5 +1,7 @@
 import { Link } from "react-router-dom"
 
+
+import imglogo from '../../assets/imagens/imglogo.svg'
 import maps from '../../assets/imagens/maps.png'
 import tel from '../../assets/imagens/tel.png'
 import hospital from '../../assets/imagens/hospital.png'
@@ -10,8 +12,14 @@ const Footer = () => {
     return (
         <>
             <footer>
-                <div className="bg-[#417571] w-full h-128 p-5 space-y-2">
-                    <div className="w-full flex flex-col text-center">
+                <div className="bg-[#417571] w-full h-auto py-7 md:flex flex-row justify-between">
+
+                    <Link to="/" className="flex items-center justify-center p-6 lg:text-xl">
+                        <img src={imglogo} alt="Página inicial do site SaúdeBot" className="w-6 h-6" />
+                        SaúdeBot
+                    </Link>
+
+                    <div className="flex flex-col text-center p-3 md:text-left">
                         <p className="text-2xl font-inter text-white">Projeto</p>
                         <ul>
                             <li className="text-white text-base font-inter">
@@ -29,7 +37,7 @@ const Footer = () => {
                         </ul>
                     </div>
 
-                    <div className="w-full flex flex-col text-center">
+                    <div className="flex flex-col text-center p-3 md:text-left px-6">
                         <p className="text-2xl font-inter text-white">Suporte</p>
                         <ul>
                             <li className="text-white text-base font-inter">
@@ -44,23 +52,23 @@ const Footer = () => {
                         </ul>
                     </div>
 
-                    <div className="w-full flex flex-col text-center space-y-2">
+                    <div className="flex flex-col text-center space-y-2 p-3 md:text-left">
                         <p className="text-2xl font-inter text-white">Informações</p>
-                        <div className="w-full flex items-center justify-center">
+                        <div className="w-full flex items-center justify-center md:justify-start">
                             <img src={maps} alt="Endereço do Hospital das Clínicas" className="w-6 h-6" />
-                            <p className="text-white text-base font-inter">Eixo Rosa, Complexo do HC, Portão 3 – Inrad, São Paulo, SP – CEP 05403-000</p>
+                            <p className="text-white text-base font-inter">Complexo do HC – Eixo Rosa (Portão 3, Inrad), São Paulo – SP</p>
                         </div>
-                        <div className="w-full flex items-center justify-center">
+                        <div className="w-full flex items-center justify-center md:justify-start">
                             <img src={tel} alt="Telefone para contato com Hospital das Clínicas" className="w-6 h-6" />
                             <p className="text-white text-base font-inter">(11) 2661-7557 / 2661-7821</p>
                         </div>
-                        <div className="w-full flex items-center justify-center">
+                        <div className="w-full flex items-center justify-center md:justify-start">
                             <img src={hospital} alt="Site oficial do Hospital das Clínicas" className="w-6 h-6" />
                             <p className="text-white text-base font-inter">
                                 Site oficial do <a href="https://redelucymontoro.org.br/site/area-do-paciente/teleatendimento/" className="text-[#1D72B8]">HC ImREA</a>
                             </p>
                         </div>
-                        <div className="w-full flex items-center justify-center">
+                        <div className="w-full flex items-center justify-center md:justify-start">
                             <img src={playstore} alt="Link da PlayStore para baixar o app Portal do Paciente - HC" className="w-6 h-6" />
                             <p className="text-white text-base font-inter">
                                 Clique <a href="https://play.google.com/store/apps/details?id=com.netihc.portaldopaciente" className="text-[#1D72B8]">aqui</a> para baixar na PlayStore
